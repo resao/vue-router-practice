2e594a48a0cec61a5e8ca8760079eab74ca97f14
Christopher Miles <mail@christophermiles.co.uk>
2018-06-04 16:27:17 +0100

    Adding named route for user edit

--- src/components/user/UserDetail.vue
+++ src/components/user/UserDetail.vue
@@ -2,5 +2,9 @@
     <div>
         <h3>Some User Details</h3>
         <p>User loaded has ID: {{ $route.params.id }}</p>
+        <router-link 
+            tag="button" 
+            :to="{ name: 'userEdit' }"
+            class="btn btn-primary">Edit user</router-link>
     </div>
 </template>
