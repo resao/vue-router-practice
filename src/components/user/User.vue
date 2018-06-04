3c96b21cfa78dcb5c2508379aaacdcd92ce61d0e
Christopher Miles <mail@christophermiles.co.uk>
2018-06-04 14:55:09 +0100

    Passing a dynamic parameter in route (id) and accessing from component

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -2,12 +2,18 @@
     <div>
         <h1>The User Page</h1>
         <hr>
-        <button @click="navigateToHome" class="btn btn-primary">Go to Home</button>
+        <p>Loaded User ID: {{id}}</p>
+        <button @click="navigateToHome" class="btn btn-primary">Go to Home</button> 
     </div>
 </template>
 
 <script>
     export default {
+        data() {
+            return {
+                id: this.$route.params.id
+            }
+        },
         methods: {
             navigateToHome(){
                 this.$router.push('/');
