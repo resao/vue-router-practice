e9d6ae545882f63cba987843c5e03143709a39fc
Christopher Miles <mail@christophermiles.co.uk>
2018-06-04 16:34:26 +0100

    Added example of passing and reading query params

--- src/components/user/UserDetail.vue
+++ src/components/user/UserDetail.vue
@@ -4,7 +4,7 @@
         <p>User loaded has ID: {{ $route.params.id }}</p>
         <router-link 
             tag="button" 
-            :to="{ name: 'userEdit' }"
+            :to="{ name: 'userEdit', query: { locale: 'en', q: 100 } }"
             class="btn btn-primary">Edit user</router-link>
     </div>
 </template>
