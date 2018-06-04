89e586e98a5e56b4b37466a9f1fb4f8c18b8f4ae
Christopher Miles <mail@christophermiles.co.uk>
2018-06-04 17:18:41 +0100

    Scroll behaviour updated
    Scroll to saved position from history, if not then to hash / id, if not then to top

--- src/main.js
+++ src/main.js
@@ -9,9 +9,13 @@ const router = new VueRouter({
   routes,
   mode: 'history',
   scrollBehavior(to, from, savedPosition){
+    if(savedPosition) {
+      return savedPosition;
+    }
     if(to.hash) {
       return { selector: to.hash }
     }
+    return { x: 0, y: 0}
   }
 });
 
